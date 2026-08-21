e44ed87f4d2df4c27a4a435b46d743739811bb64
Taran Warren <taranwarren6@gmail.com>
2026-08-21 13:37:45 +0100

    create sqlite3 database with necesary columns

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
 print("helllo world")
+import sqlite3
+connection = sqlite3.connect("objects.db")
+cursor = connection.cursor()
+cursor.execute("""create table if not exists objects(id integer primary key autoincrement,user_id text, label text,embedding blob,thumnail_path text, gifter_name text)""")
