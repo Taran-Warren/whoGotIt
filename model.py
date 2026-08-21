3c0716b9ba37b17e41e38528ce9bf52098b2405e
Taran Warren <taranwarren6@gmail.com>
2026-08-21 22:45:18 +0100

    Add additional test cases

--- model.py
+++ model.py
@@ -5,22 +5,28 @@ import open_clip
 from PIL import Image
 from mobileclip.modules.common.mobileone import reparameterize_model
 
-model, _, preprocess = open_clip.create_model_and_transforms('MobileCLIP2-S0', pretrained=r'C:\Users\Warren\.cache\huggingface\hub\models--apple--MobileCLIP2-S2\snapshots\72424e7025436db18f15c3eff6ee8c7c15ad4481\mobileclip2_s2.pt')
-#model, _, preprocess = open_clip.create_model_and_transforms('MobileCLIP2-S0', pretrained=r'C:\Users\Warren\.cache\huggingface\hub\models--apple--MobileCLIP2-S0\snapshots\3136ea51c8ed56b9f9abfab04cb816735aaad6cb\mobileclip2_s0.pt')
+#model, _, preprocess = open_clip.create_model_and_transforms('MobileCLIP2-S2', pretrained=r'C:\Users\Warren\.cache\huggingface\hub\models--apple--MobileCLIP2-S2\snapshots\72424e7025436db18f15c3eff6ee8c7c15ad4481\mobileclip2_s2.pt')
+model, _, preprocess = open_clip.create_model_and_transforms('MobileCLIP2-S0', pretrained=r'C:\Users\Warren\.cache\huggingface\hub\models--apple--MobileCLIP2-S0\snapshots\3136ea51c8ed56b9f9abfab04cb816735aaad6cb\mobileclip2_s0.pt')
 model = reparameterize_model(model)
 model.eval()
 image1 = preprocess(Image.open("test_1a.jpg")).unsqueeze(0)
 image2 = preprocess(Image.open("test_1b.jpg")).unsqueeze(0)
 image3 = preprocess(Image.open("test_2a.jpg")).unsqueeze(0)
 image4 = preprocess(Image.open("test_3a.jpg")).unsqueeze(0)
+image5 = preprocess(Image.open("test_1c.png")).unsqueeze(0)
+image6 = preprocess(Image.open("test_2c.png")).unsqueeze(0)
+image7 = preprocess(Image.open("test_1d.png")).unsqueeze(0)
 
 with torch.no_grad():
     embedding1 = model.encode_image(image1)
     embedding2 = model.encode_image(image2)
     embedding3 = model.encode_image(image3)
     embedding4 = model.encode_image(image4)
+    embedding5 = model.encode_image(image5)
+    embedding6 = model.encode_image(image6)
+    embedding7 = model.encode_image(image7)
 
-similarity_same = torch.nn.functional.cosine_similarity(embedding1, embedding2)
-similarity_diff = torch.nn.functional.cosine_similarity(embedding1, embedding4)
+similarity_same = torch.nn.functional.cosine_similarity(embedding5, embedding7)
+similarity_diff = torch.nn.functional.cosine_similarity(embedding5, embedding4)
 print("Same object similarity:", similarity_same.item())
 print("Different object similarity:", similarity_diff.item())
