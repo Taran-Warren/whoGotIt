48aa418c1d1736613e105e6782c83843e64237f3
Taran Warren <taranwarren6@gmail.com>
2026-08-21 14:48:18 +0100

    add test columns

--- main.py
+++ main.py
@@ -2,4 +2,6 @@ print("helllo world")
 import sqlite3
 connection = sqlite3.connect("objects.db")
 cursor = connection.cursor()
-cursor.execute("""create table if not exists objects(id integer primary key autoincrement,user_id text, label text,embedding blob,thumnail_path text, gifter_name text)""")
+cursor.execute("""create table if not exists objects(id integer primary key autoincrement,user_id text, label text,embedding blob,thumbnail_path text,gifter_name text)""")
+cursor.execute(""" insert into objects(user_id,label,embedding,thumbnail_path,gifter_name)values(?,?,?,?,?)""",(123,"lab",424553535,"imagge","tom"))
+connection.commit()
