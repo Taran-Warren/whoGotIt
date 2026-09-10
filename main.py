1c3102e78c9dd0cc32e675c0fa96cd38b848d9ec
Taran Warren <taranwarren6@gmail.com>
2026-09-10 21:59:14 +0100

    add SQL get objects function

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 print("helllo world")
 import sqlite3
+import numpy
 connection = sqlite3.connect("objects.db")
 cursor = connection.cursor()
 cursor.execute("""create table if not exists objects(id integer primary key autoincrement,user_id text, label text,embedding blob,thumbnail_path text,gifter_name text)""")
 cursor.execute(""" insert into objects(user_id,label,embedding,thumbnail_path,gifter_name)values(?,?,?,?,?)""",(123,"lab",424553535,"imagge","tom"))
 connection.commit()
+
+
+def get_all_objects():
+    cursor.execute("""SELECT * FROM objects""")
+    rows = cursor.fetchall()
+    return rows
+
+
+
+objects = get_all_objects()
+print (objects)
